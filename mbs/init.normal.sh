df205456e3ef097127e63626b39fb73400af8585
sakuramilk <c.sakuramilk@gmail.com>
2012-02-15 00:33:25 +0900

    revert ics support

--- mbs/init.normal.sh
+++ mbs/init.normal.sh
@@ -268,21 +268,21 @@ func_vender_init()
 	chown system.system $BOOT_ROM_DATA_PATH
 
 	# android version code 9 or 10 is gingerbread, 14 or 15 is icecreamsandwitch
-	SDK_VER=`grep ro\.build\.version\.sdk $ROM_SYS_PATH/build.prop | cut -d'=' -f2`
-	if [ "$SDK_VER" = '14' -o "$SDK_VER" = '15' ]; then
-		ANDROID_VER=ics
-    else
-		ANDROID_VER=gb
-    fi
+	#SDK_VER=`grep ro\.build\.version\.sdk $ROM_SYS_PATH/build.prop | cut -d'=' -f2`
+	#if [ "$SDK_VER" = '14' -o "$SDK_VER" = '15' ]; then
+	#	ANDROID_VER=ics
+    #else
+	#	ANDROID_VER=gb
+    #fi
 
-	sh /mbs/init.common.sh $ANDROID_VER
+	#sh /mbs/init.common.sh $ANDROID_VER
 
 	if [ -f $ROM_SYS_PATH/framework/twframework.jar ]; then
 		ROM_VENDOR=samsung
-		sh /mbs/init.samsung.sh $ANDROID_VER $ROM_SYS_PATH $BOOT_ROM_DATA_PATH
+		sh /mbs/init.samsung.sh $ROM_SYS_PATH $BOOT_ROM_DATA_PATH
 	else
 		ROM_VENDOR=aosp
-		sh /mbs/init.aosp.sh $ANDROID_VER $ROM_SYS_PATH $BOOT_ROM_DATA_PATH
+		sh /mbs/init.aosp.sh $ROM_SYS_PATH $BOOT_ROM_DATA_PATH
 	fi
 	echo ROM_VENDOR=$ROM_VENDOR >> $MBS_LOG
 	cp /mbs/init.rc.temp /xdata/init.rc.temp
