ee2fd0328eeb36669f1da8692980d2c69eee1bca
sakuramilk <c.sakuramilk@gmail.com>
2012-04-15 04:42:53 +0900

    change TweakGS2 proprties

--- mbs/init.tgs2.sh
+++ mbs/init.tgs2.sh
@@ -2,12 +2,12 @@
 
 PROP_PATH=$1/tweakgs2.prop
 
-USB_CONFIG=`grep persist\.sys\.usb\.config $PROP_PATH | cut -d'=' -f2`
-if [ -n "$USB_CONFIG" ]; then
-  echo persist.sys.usb.config=$USB_CONFIG >> /default.prop
-else
+#USB_CONFIG=`grep persist\.sys\.usb\.config $PROP_PATH | cut -d'=' -f2`
+#if [ -n "$USB_CONFIG" ]; then
+#  echo persist.sys.usb.config=$USB_CONFIG >> /default.prop
+#else
   echo persist.sys.usb.config=mass_storage,adb >> /default.prop
-fi
+#fi
 
 BOOT_SND=`grep audioflinger\.bootsnd $PROP_PATH | cut -d'=' -f2`
 if [ "$BOOT_SND" = '1' ]; then
@@ -28,4 +28,24 @@ if [ -z "$LCD_DENSITY" ]; then
   LCD_DENSITY=240
 fi
 echo ro.sf.lcd_density=$LCD_DENSITY >> /default.prop
+
+LOGGER=`grep ro\.tgs2\.logger $PROP_PATH | cut -d'=' -f2`
+if [ "$LOGGER" = '1' ]; then
+  insmod /lib/modules/logger.ko
+fi
+
+CIFS=`grep ro\.tgs2\.cifs $PROP_PATH | cut -d'=' -f2`
+if [ "$CIFS" = '1' ]; then
+  insmod /lib/modules/cifs.ko
+fi
+
+NTFS=`grep ro\.tgs2\.ntfs $PROP_PATH | cut -d'=' -f2`
+if [ "$NTFS" = '1' ]; then
+  insmod /lib/modules/ntfs.ko
+fi
+
+J4FS=`grep ro\.tgs2\.j4fs $PROP_PATH | cut -d'=' -f2`
+if [ "$J4FS" = '1' ]; then
+  insmod /lib/modules/j4fs.ko
+fi
 
