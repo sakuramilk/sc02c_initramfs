860d3057476d612448d9e0d9219678601c842f0a
Masashi <ma34s@zvw40.(none)>
2012-07-19 23:56:25 +0900

    update bootanimation (bootmovie)

--- sbin/bootanimation.sh
+++ sbin/bootanimation.sh
@@ -1,8 +1,11 @@
 #!/sbin/busybox sh
 
+
 if [ -f /data/local/bootanimation.bin ]; then
   /data/local/bootanimation.bin
 elif [ -f /data/local/bootanimation.zip ] || [ -f /system/media/bootanimation.zip ]; then
+  /sbin/bootanimation
+elif [ -f /data/local/bootmovie.mp4 ] ; then
   /sbin/bootanimation
 else
   /system/bin/samsungani
