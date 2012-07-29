34ecf2b813a94680d73c701acf567bb081dd2dec
ma34s.sss@gmail.com <ma34s@zvw40.(none)>
2012-07-30 00:31:16 +0900

    for multi (not mbs)

--- mbs/init.aosp-ics.sh
+++ mbs/init.aosp-ics.sh
@@ -1,5 +1,10 @@
 #!/sbin/busybox sh
 
+#set feature_aosp
+mount -t proc proc /proc
+echo 1 > /proc/sys/kernel/feature_aosp
+umount /proc
+
 # copy rc files
 cp /mbs/aosp-ics/default.prop /
 cp /mbs/aosp-ics/init.smdk4210.rc /
